3fb78f434425a42731c402f2bf0d94d459d30e91
JOSH <149009251+xxxJoshyBOI123@users.noreply.github.com>
2024-10-10 13:45:07 -0400

    put new scanner

--- Diamond.java
+++ Diamond.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
     System.out.println("Enter a number: ");
+    Scanner input = new Scanner(System.in);
   }
 }
