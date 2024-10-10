d5e3a8bb18789fcec6568baf229a96120b9ae9e3
JOSH <149009251+xxxJoshyBOI123@users.noreply.github.com>
2024-10-10 13:48:56 -0400

    added int num

--- Diamond.java
+++ Diamond.java
@@ -4,5 +4,6 @@ public class Diamond {
   public static void main(String[] args) {
     System.out.println("Enter a number: ");
     Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
   }
 }
