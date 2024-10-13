1e71f38139748a49c432847e3e232b7444042508
JOSH <149009251+xxxJoshyBOI123@users.noreply.github.com>
2024-10-13 00:40:20 -0400

    for loop (bottom stars)

--- Diamond.java
+++ Diamond.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
 
 public class Diamond {
-  public static void main(String[] args) {
-    System.out.println("Enter a number: ");
-    Scanner input = new Scanner(System.in);
-    int num = input.nextInt();
+   public static void main(String[] args) {
+      System.out.println("Enter a number: ");
+      Scanner input = new Scanner(System.in);
+      int num = input.nextInt();
 
-    int count = 1;
-    for(int topspaces = 1; topspaces < num; topspaces++);{
-      System.out.print(" ");
+      int count = 1;
+      for(int topspaces = 1; topspaces < num; topspaces++);{
+        System.out.print(" ");
 
-    }
-    for(int topstars = 1; topstars < num; topstars++);
-      System.out.print("*");
+      }
+      for(int topstars = 1; topstars < num; topstars++); {
+        System.out.print("*");
+      }
 
-  }
-  System.out.println();
+    System.out.println();
+
+      for(int botspaces = 1; botspaces < num; botspaces++); {
+        System.out.print(" ");
+     }
+     for(int botstars = 1; botstars < num; botstars++); {
+        System.out.print("*");
+
+     }
+    System.out.println();
+   }
 }
